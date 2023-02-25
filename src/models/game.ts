c085e696d9955cca24657665a8005450f6fb8af1
FabianFle <fabianflegler1@yahoo.de>
2023-02-25 14:06:11 +0100

    bugg fixed

--- src/models/game.ts
+++ src/models/game.ts
@@ -5,7 +5,7 @@ export class Game {
     public currentPlayer: number = 0;
 
     constructor() {
-        for (let i = 0; 1 < 14; i++) {
+        for (let i = 1; i < 14; i++) {
             this.stack.push('ace_' + i);
             this.stack.push('clubs_' + i);
             this.stack.push('diamonds_' + i);
@@ -17,9 +17,12 @@ export class Game {
 
 function shuffel(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
+    
     while (0 !== currentIndex) {
+
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
+
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
